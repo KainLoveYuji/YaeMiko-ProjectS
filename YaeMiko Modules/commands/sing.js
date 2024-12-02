e89a5f1b683a3fb30f30611f619900c89f4e39f3
KainLoveYuji <lek93226@gmail.com>
2024-12-02 06:43:10 +0000

    Initial commit

--- YaeMiko Modules/commands/sing.js
+++ YaeMiko Modules/commands/sing.js
@@ -1,5 +1,5 @@
 const fs = require('fs-extra');
-const ytdl = require('ytdl-core');
+const ytdl = require('@distube/ytdl-core');
 const Youtube = require('youtube-search-api');
 const convertHMS = (value) => new Date(value * 1000).toISOString().slice(11, 19);
 
